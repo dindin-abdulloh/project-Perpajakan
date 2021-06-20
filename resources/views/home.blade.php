7d2520e15e7283ae40601bee095e8ac8356095a2
dindin-abdulloh <50541680+adin17-alt@users.noreply.github.com>
2021-06-20 16:02:59 +0700

    templating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,12 @@
 @extends('layouts.app')
-   
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                <div class="card-body">
-                    You are normal user.
-                </div>
-            </div>
+        <div class="col-md-12">
+           <div class="container">
+                <div class="title"><h3>SURAT PEMBERITAHUAN PAJAK TERHUTANG</h3></div>
+           </div>
         </div>
     </div>
 </div>
